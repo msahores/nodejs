6e27e2ac92b2bb1e516d0ebd21b7350188f45c9e
manuelsahores <msahores@basicagency.com>
2020-03-24 21:38:28 -0300

    Fix

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   let {url, method, headers} = req;
 
-  if(url = '/'){
+  if(url === '/'){
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">SEND</button></form></body>');
     res.write('</html>');
     return res.end();
   }
-  if(url = '/message' && method === 'POST'){
+  if(url === '/message' && method === 'POST'){
     fs.writeFileSync('message.txt', 'DUMMY');
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
   }
-
+  
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Node Page</title></head>');
