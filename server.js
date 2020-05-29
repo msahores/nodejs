ef620bb532a887ae3e29e7c7cccb7545f66f0c0c
manuel <manuel@sahores.com>
2020-05-29 18:31:29 -0300

    basic routing with middlewares

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-  console.log('In the middleware');
-  next();
+app.use('/add-product', (req, res, next)=>{
+  console.log('Add a Product Middleware');
+  res.send('<h1>Add a Product</h1>')
 })
 
-app.use((req, res, next)=>{
+app.use('/', (req, res, next)=>{
   console.log('In another middleware');
   res.send('<h1>Hello From Express!</h1>')
 })
