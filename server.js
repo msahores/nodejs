d353085b16b2499f5096bfe8af94d1536a834d55
manuel <manuel@sahores.com>
2020-07-01 19:11:52 -0300

    Parsing Incoming requests

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
 
 app.use('/add-product', (req, res, next)=>{
-  console.log('Add a Product Middleware');
-  res.send('<h1>Add a Product</h1>')
+  res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Send</button></form>')
+})
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/')
 })
 
 app.use('/', (req, res, next)=>{
-  console.log('In another middleware');
   res.send('<h1>Hello From Express!</h1>')
 })
 
